cc21f3338f417222c44f98173f6e1128da7746c1
TecaHas <teacahashimoto@gmail.com>
2025-06-19 13:53:37 -0300

    Update server.js

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.static('public'));
 
-app.listen(PORT, () => {
-  console.log(`Servidor rodando na porta ${PORT}`);
+const port = process.env.PORT || 10000;
+app.listen(port, () => {
+  console.log(`Servidor rodando na porta ${port}`);
 });
+
